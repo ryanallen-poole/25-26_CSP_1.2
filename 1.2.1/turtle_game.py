a07bfcc8e77461b8063c003a9fb122b908a85a97
Ryan A <ryan.allen-poole@vtsd.com>
2025-12-04 13:55:22 -0500

    turtle_game.py

--- 1.2.1/turtle_game.py
+++ 1.2.1/turtle_game.py
@@ -5,6 +5,7 @@ import random as rand
 
 #-----game configuration----
 spot_color = "purple"
+score = 0
 
 #-----initialize turtle-----
 meowl = trtl.Turtle()
@@ -12,16 +13,26 @@ meowl.shape("circle")
 meowl.color(spot_color)
 meowl.shapesize(3)
 meowl.penup()
-
 #-----game functions--------
 #get a score boost,
 def spot_clicked(x,y):
-    #move the turtle to a random location
-    newX = rand.randint(-450,450)
-    newY = rand.randint(-450,450)
-    meowl.goto(newX,newY)
+  update_score()
+  change_position()
+
+def change_position():
+    # move the turtle to a random location
+    newX = rand.randint(-450, 450)
+    newY = rand.randint(-450, 450)
+    meowl.goto(newX, newY)
 
 
+def update_score():
+    #include global score
+        global score
+    #increament the score by 1
+        score += 1
+    #print the score
+        print(score)
 #-----events----------------
 meowl.onclick(spot_clicked)
 
