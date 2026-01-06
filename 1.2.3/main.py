8ce4b64d8a8cba066628ff91cc8e5e56fd274995
Ryan A <ryan.allen-poole@vtsd.com>
2026-01-06 11:46:23 -0500

    1.2.3

--- 1.2.3/main.py
+++ 1.2.3/main.py
@@ -1,39 +1,43 @@
-#   a123_apple_1.py
 import turtle as trtl
 
-#-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
+ground_height = -200
+apple_letter_x_offset = -25
+apple_letter_y_offset = -50
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
 
-apple = trtl.Turtle()
+alphabet_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-#-----functions-----
+wn.bgpic("background.gif")
+apple = trtl.Turtle()
+wn.tracer(False)
+apple.penup()
+
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
-  wn.update()
-drawer = trtl.Turtle()
-
+  draw_letter("A", active_apple)
 
 def drop_apple():
-    apple.goto(100,-200)
+    wn.tracer(True)
+    apple.goto(apple.xcor(), ground_height)
+    apple.clear()
+    apple.hideturtle()
+    wn.tracer(False)
 
-def draw_letter(letter, active_apple):...
+
+def draw_letter(letter, active_apple):
+  active_apple.color("white")
+  remember_position = active_apple.position()
+  active_apple.setpos(active_apple.xcor() + apple_letter_x_offset,active_apple.ycor() + apple_letter_y_offset)
+  active_apple.write(letter, font=("Arial", 74, "bold"))
+  active_apple.setpos(remember_position)
 
 draw_apple(apple)
-wn.onkeypress()
-# This call to the onkeypress function sets draw_an_A as the function
-# that will be called when the "a" key is pressed.
-
-
-#-----function calls-----
-wn.onkeypress(draw_an_A, "a")
+wn.onkeypress(drop_apple, "a")
 
 wn.listen()
-
-draw_apple(apple)
-
-wn.mainloop()
+trtl.mainloop()
