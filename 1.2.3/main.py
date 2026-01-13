3360b63f134df7a75daafa5b0ca4b2b378fec709
Ryan A <ryan.allen-poole@vtsd.com>
2026-01-13 13:42:00 -0500

    1.2.3

--- 1.2.3/main.py
+++ 1.2.3/main.py
@@ -31,8 +31,8 @@ def reset_apple(active_apple):
         index = rand.randint(0, length_of_list)
         current_letter = letters.pop(index)
         # Finish This First
-        active_apple.goto(rand.randint(-(screen_width)/2, (screen_width)/2),
-                          rand.randint(-(screen_height) / 2, (screen_height) / 2))
+        #active_apple.goto(rand.randint(-(screen_width)/2, (screen_width)/2),
+                          #rand.randint(-(screen_height) / 2, (screen_height) / 2))
         active_apple.goto(200,200)
         draw_apple(active_apple, current_letter)
 
@@ -66,38 +66,112 @@ def draw_letter(letter, active_apple):
 def check_apple_a():
     if(current_letter == "a"):
         drop_apple()
-
+def check_apple_b():
+    if(current_letter == "b"):
+        drop_apple()
+def check_apple_c():
+    if(current_letter == "c"):
+        drop_apple()
+def check_apple_d():
+    if(current_letter == "d"):
+        drop_apple()
+def check_apple_e():
+    if(current_letter == "e"):
+        drop_apple()
+def check_apple_f():
+    if(current_letter == "f"):
+        drop_apple()
+def check_apple_g():
+    if(current_letter == "g"):
+        drop_apple()
+def check_apple_h():
+    if(current_letter == "h"):
+        drop_apple()
+def check_apple_i():
+    if(current_letter == "i"):
+        drop_apple()
+def check_apple_j():
+    if(current_letter == "j"):
+        drop_apple()
+def check_apple_k():
+    if(current_letter == "k"):
+        drop_apple()
+def check_apple_l():
+    if(current_letter == "l"):
+        drop_apple()
+def check_apple_m():
+    if(current_letter == "m"):
+        drop_apple()
+def check_apple_n():
+    if(current_letter == "n"):
+        drop_apple()
+def check_apple_o():
+    if(current_letter == "o"):
+        drop_apple()
+def check_apple_p():
+    if(current_letter == "p"):
+        drop_apple()
+def check_apple_q():
+    if(current_letter == "q"):
+        drop_apple()
+def check_apple_r():
+    if(current_letter == "r"):
+        drop_apple()
+def check_apple_s():
+    if(current_letter == "s"):
+        drop_apple()
+def check_apple_t():
+    if(current_letter == "t"):
+        drop_apple()
+def check_apple_u():
+    if(current_letter == "u"):
+        drop_apple()
+def check_apple_v():
+    if(current_letter == "v"):
+        drop_apple()
+def check_apple_w():
+    if(current_letter == "w"):
+        drop_apple()
+def check_apple_x():
+    if(current_letter == "x"):
+        drop_apple()
+def check_apple_y():
+    if(current_letter == "y"):
+        drop_apple()
+def check_apple_z():
+    if(current_letter == "z"):
+        drop_apple()
 
 draw_apple(apple,current_letter)
 
 wn.onkey(check_apple_a, "a")
 wn.onkey(check_apple_b, "b")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
-wn.onkey(check_apple_a, "a")
+wn.onkey(check_apple_c, "c")
+wn.onkey(check_apple_d, "d")
+wn.onkey(check_apple_e, "e")
+wn.onkey(check_apple_f, "f")
+wn.onkey(check_apple_g, "g")
+wn.onkey(check_apple_h, "h")
+wn.onkey(check_apple_i, "i")
+wn.onkey(check_apple_j, "j")
+wn.onkey(check_apple_k, "k")
+wn.onkey(check_apple_l, "l")
+wn.onkey(check_apple_m, "m")
+wn.onkey(check_apple_n, "n")
+wn.onkey(check_apple_o, "o")
+wn.onkey(check_apple_p, "p")
+wn.onkey(check_apple_q, "q")
+wn.onkey(check_apple_r, "r")
+wn.onkey(check_apple_s, "s")
+wn.onkey(check_apple_t, "t")
+wn.onkey(check_apple_u, "u")
+wn.onkey(check_apple_v, "v")
+wn.onkey(check_apple_w, "w")
+wn.onkey(check_apple_x, "x")
+wn.onkey(check_apple_y, "y")
 wn.onkey(check_apple_z, "z")
 
 
-wn.onkeypress(drop_apple, "a")
 
 wn.listen()
+trtl.mainloop()
